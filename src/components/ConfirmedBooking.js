bdda9297b7b32575a1c87fd92412c32e130f6d1f
adil-khursheed <adilkhursheed60@gmail.com>
2023-01-28 11:50:23 +0530

    improved confirmation page

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -4,9 +4,9 @@ import './ConfirmedBooking.css'
 const ConfirmedBooking = () => {
   return (
     <>
-      <main>
-        <h2>Thank You</h2>
-        <p>Your reservation has been completed successfully.</p>
+      <main className='confirmation__page'>
+        <h2 className='confirmation__title'>Thank You</h2>
+        <p className='confirmation__desc'>Your reservation has been completed successfully.</p>
       </main>
     </>
   )
